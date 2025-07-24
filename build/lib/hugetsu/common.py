da272a2e206cf89e88322d2b75a2dc648fdfcc58
PEN <uemasaki20011222@gmail.com>
2025-07-25 01:33:25 +0900

    hugetsu

--- build/lib/hugetsu/common.py
+++ build/lib/hugetsu/common.py
@@ -6,7 +6,7 @@ class Unit():
 | name          | str   | 単位名                    |
 | ratio         | float | SI単位系に対する比率      |
 | type          | str   | 単位の種類を区別する一意の値 |
-| content       | str   | SI単位系で表した時の値 |
+| content       | float | SI単位系で表した時の値 |
 
 使用可能メソッド  
 | Method        | type  | args      | description                   |
